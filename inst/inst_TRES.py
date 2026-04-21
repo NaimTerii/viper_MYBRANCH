20069af55597073d5ab9db54d81c28ea82582994
Naïm Teriitehau-Martin <naimteriitehau@gmail.com>
2026-04-21 17:27:05 +0200

    Update inst_TRES.py

--- inst/inst_TRES.py
+++ inst/inst_TRES.py
@@ -70,11 +70,6 @@ def Spectrum(filename='', order=None, targ=None):
 
 def Tpl(tplname, order=None, targ=None):
     '''Tpl should return barycentric corrected wavelengths'''
-
-    # Check if template already convolved with the IP (If yes, will need to apply a different convolution in utils/model.py) ;
-    # Currently not applied to this instrument ; will need to add in the future if required
-    global tpl_IP_isconv    # define as global so it can be called from other files
-    tpl_IP_isconv = False    # Insert some condition to check if True or False (e.g. check inst/CARM_VIS.py)
     
     wave, spec = read_tpl(tplname, inst=os.path.basename(__file__), order=order, targ=targ) 
 
